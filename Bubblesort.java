c8a9629d52e62c7b7f9cd33a7651b2ab16281809
Kadavi Savage <coedry@gmail.com>
2012-09-03 02:30:30 -0700

    Initial commit.

---
+++ Bubblesort.java
@@ -0,0 +1,38 @@
+public class Bubblesort {
+
+    public static int[] bubblesort(int[] nums) {
+        boolean flag = true;
+
+        while (flag) {
+
+            flag = false;
+
+            for (int i = 0; i < nums.length - 1; i++) {
+
+                if (nums[i] > nums[i + 1]) {
+                    int temp = nums[i];
+                    nums[i] = nums[i + 1];
+                    nums[i + 1] = temp;
+                    flag = true;
+                }
+            }
+        }
+
+        return nums;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {81, 1, 3, 5};
+        
+        bubblesort(nums);
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+		// Output: 1 3 5 81 
+
+    }
+}
